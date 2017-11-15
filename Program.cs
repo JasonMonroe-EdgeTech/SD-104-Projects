39187406e6731f75546c8d99194ac6ac538564a9
Jason Monroe <jmonroe@ancoraeducation.com>
2017-11-15 15:05:41 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ *  Project: Lab 2 Batting Average
+ *     Date: 11-13-2017
+ *   Author: J.Monroe
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2___Batting_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAverage;
+
+            //prompt user 
+            Console.WriteLine("---Calculate Batting Average---");
+            Console.Write("Enter the player's name: ");
+            strPlayerName = Console.ReadLine();
+
+            //prompt user for hits
+            Console.Write("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+            //prompt user for number of at bats
+            Console.Write("Enter the player's # of at bats: ");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            dblBattingAverage = (double)nHits / nAtBats;
+
+            //display results
+            Console.WriteLine(strPlayerName + "'s batting average is " + dblBattingAverage);
+            Console.ReadLine();
+
+
+        }
+    }
+}
