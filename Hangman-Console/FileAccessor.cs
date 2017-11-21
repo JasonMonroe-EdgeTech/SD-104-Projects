c6ed7a1d07a701f355910927e70f590b6cb93496
JasonMonroe-EdgeTech <jmonroe@ancoraeducation.com>
2017-11-20 21:36:55 -0600

    Add files via upload

---
+++ Hangman-Console/FileAccessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Console
+{
+    class FileAccessor
+    {
+        /// <summary>
+        /// Gets or sets the path to the file that is being accessed.
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Creates an instance of a FileAccessor, setting
+        /// the FilePath property to the passed value.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public FileAccessor(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        /// <summary>
+        /// Returns a random line from the file.
+        /// </summary>
+        /// <returns>Random line from file as a string.</returns>
+        public string GetRandomLine()
+        {
+            StreamReader textIn = new StreamReader(FilePath);
+
+            //get random word
+            Random random = new Random();
+            int randNum = random.Next(1, 479781);
+
+            //read through file to get that word
+            for (int n = 0; n < randNum; n++)
+            {
+                textIn.ReadLine();
+            }
+
+            return textIn.ReadLine();
+
+        }
+    }
+}
---
+++ Hangman-Console/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Console
+{
+    class Program
+    {
+        static string strLettersGuessed;
+        static string strSecretWord = GetWord();
+        static int nWrongGuesses = 0;
+        static string[] WordDisplay = InitializeWordDisplay(strSecretWord);
+        static bool guessedIt = false;
+
+
+        static void Main(string[] args)
+        {
+            //display all gallows
+            while(!guessedIt && nWrongGuesses < 10)
+            {
+                HangmanGallows.DrawGallow(nWrongGuesses);
+                //print WordDisplay
+                PrintSecretWord(WordDisplay);
+                Console.WriteLine("Letters guessed: " + strLettersGuessed);
+                nWrongGuesses += PlayerGuess(WordDisplay, strSecretWord);
+                guessedIt = GuessedWord(WordDisplay);
+            }//end While
+
+            if(guessedIt)
+            {
+                Console.WriteLine("You got it!! " + strSecretWord);
+            }
+            else
+            {
+                Console.WriteLine("Executed!! The word was " + strSecretWord);
+            }
+
+            Console.ReadLine();
+
+        }//end Main
+
+    
+
+        private static bool GuessedWord(string[] WordDisplay)
+        {
+            //loop through WordDisplay and see if the word is guessed
+            for (int n = 0; n < WordDisplay.Length; n++)
+            {
+                if(WordDisplay[n] == " _ ")
+                {
+                    return false;
+                }//end if
+            }//end for loop
+
+            return true;
+        }//end GuessedWord
+
+        private static int PlayerGuess(string[] WordDisplay, string strSecretWord)
+        {
+            string strUserGuess;
+            int nWrongGuess = 1;
+
+            //prompt user
+            Console.Write("\nGuess a letter:");
+            strUserGuess = Console.ReadLine();
+            strLettersGuessed += strUserGuess;
+
+            //process guess
+            for (int n = 0; n < strSecretWord.Length; n++)
+            {
+                if(strSecretWord[n].ToString() == strUserGuess)
+                {
+                    WordDisplay[n] = strUserGuess;
+                    nWrongGuess = 0;
+                }
+            }
+
+            return nWrongGuess;
+
+        }
+
+        private static void PrintSecretWord(string[] wordDisplay)
+        {
+            //print word display
+            for (int n = 0; n < wordDisplay.Length; n++)
+            {
+                Console.Write(wordDisplay[n]);
+            }
+
+            Console.WriteLine();
+
+        }
+
+        private static string[] InitializeWordDisplay(string strSecretWord)
+        {
+            string[] wordDisplay = new string[strSecretWord.Length];
+
+            //fill wordDisplay array with _
+            for (int n = 0; n < wordDisplay.Length; n++)
+            {
+                wordDisplay[n] = " _ ";
+            }
+
+            return wordDisplay;
+            
+        }
+
+       
+    }
+}
